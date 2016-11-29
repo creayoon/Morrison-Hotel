d0af596b6c8a05177b614905730d1f8a836d0f22
올망 <Jyoon@olmangnim.local>
2016-11-30 01:16:29 +0900

    How to upload file to aws? _socket install

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon'); // 서비스를 대표하는 아이콘
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var app = express();
+var io = require('socket.io').listen(3030);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// routing
+app.use('/', index);
+app.use('/users', users);
+
+// socket
+io.sockets.on('connect', function(socket) {
+	socket.on('call', function(data) {
+		console.log(data);
+		socket.emit('answer', 'hello php');
+	});
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); // public폴더에 파비콘 만들어 넣어서 사용
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
